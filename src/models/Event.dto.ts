101934b5a3c6eaf4efdee5b1daf04da7420c4afc
Akshat Rawat <akshat010501@gmail.com>
2025-07-24 15:54:38 +0530

    🎉 begin Nearby Events

---
+++ src/models/Event.dto.ts
@@ -0,0 +1,12 @@
+export interface Event {
+  id: string;
+  title: string;
+  description: string;
+  startTime: string;
+  endTime: string;
+  location: {
+    latitude: number;       
+    longitude: number;      
+  };
+  isJoined?: boolean;       
+}
