082ea1e3e7e963431bf8f6e08e80c649fc0be746
Akshat Rawat <akshat010501@gmail.com>
2025-07-24 17:55:11 +0530

    mock getEvents & getEventById

---
+++ src/api/eventsApi.ts
@@ -0,0 +1,29 @@
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+import { faker } from '@faker-js/faker';
+import { Event } from '../models/event.dto';
+
+const events: Event[] = Array.from({ length: 20 }).map(() => {
+  const start = faker.date.soon({ days: 10 });
+  const end = new Date(
+    start.getTime() + faker.number.int({ min: 60, max: 180 }) * 60000,
+  );
+
+  return {
+    id: uuidv4(),
+    title: faker.lorem.words({ min: 1, max: 3 }),
+    description: faker.lorem.sentences(2),
+    startTime: start.toISOString(),
+    endTime: end.toISOString(),
+    location: {
+      latitude: faker.location.latitude(),
+      longitude: faker.location.longitude(),
+    },
+    isJoined: faker.datatype.boolean(),
+  };
+});
+
+export const getEvents = async () => events;
+
+export const getEventById = async (id: string) =>
+  events.find(e => e.id === id)!;
