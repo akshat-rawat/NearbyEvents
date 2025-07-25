40838a50173285620406e8f97b56f32b79e3eb03
Akshat Rawat <akshat010501@gmail.com>
2025-07-25 14:56:22 +0530

    complete DetailScreen

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import { getEventById } from '../api/eventsApi';
 import Event from '../models/event.dto';
 import EventCard from '../components/EventCard';
+import FadeInButton from '../components/FadeInButton';
+import { ScreenProps } from '../navigation/RootNavigation';
 
-interface Props {
-  eventId: string;
-  onBackPress: () => void;
-}
-
-export default function DetailScreen({ eventId, onBackPress }: Props) {
+export default function DetailScreen({
+  route,
+  navigation,
+}: ScreenProps<'Details'>) {
+  const { eventId } = route.params;
   const [event, setEvent] = useState<Event | null>(null);
 
   useEffect(() => {
@@ -24,18 +26,54 @@ export default function DetailScreen({ eventId, onBackPress }: Props) {
     fetchEventDetails(eventId);
   }, [eventId]);
 
+  if (!event) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={onBackPress} style={styles.header}>
+      <TouchableOpacity onPress={navigation.goBack} style={styles.header}>
         <Image source={require('../assets/icons/chevron-left.png')} />
-        <Text style={styles.title}>{event?.title}</Text>
+        <Text style={styles.title}>{event.title}</Text>
       </TouchableOpacity>
-      <Image
-        source={require('../assets/images/event.png')}
-        style={styles.eventImage}
-        resizeMode="cover"
-      />
-      {event && <EventCard event={event} showBorder={false} showTime={true} />}
+
+      <View>
+        <Image
+          source={require('../assets/images/event.png')}
+          style={styles.eventImage}
+          resizeMode="cover"
+        />
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: event.location.latitude,
+              longitude: event.location.longitude,
+              latitudeDelta: 0.1,
+              longitudeDelta: 0.1,
+            }}
+          >
+            <Marker
+              coordinate={event.location}
+              image={require('../assets/icons/selected-marker.png')}
+            />
+          </MapView>
+        </View>
+      </View>
+
+      <EventCard event={event} showBorder={false} showTime={true} />
+
+      <View style={styles.buttonContainer}>
+        <FadeInButton
+          text={event.isJoined ? 'Joined' : 'Join'}
+          disabled={event.isJoined}
+          onPress={() => setEvent({ ...event, isJoined: true })}
+        />
+      </View>
     </View>
   );
 }
@@ -60,5 +98,21 @@ const styles = StyleSheet.create({
   eventImage: {
     width: '100%',
     height: 250,
+  },
+  mapContainer: {
+    position: 'absolute',
+    right: 12,
+    bottom: 12,
+  },
+  map: {
+    width: 82,
+    height: 82,
+    borderRadius: 8,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 24,
+    left: 24,
+    right: 24,
   },
 });
